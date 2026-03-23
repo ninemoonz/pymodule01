23f9482012c343b17bb1fec3fc3768cee70cf1bd
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2026-03-23 23:50:23 +0100

    ex1 on the progress

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -1,9 +1,14 @@
 class plant:
-	def __init__(self, name, height, age):
+	def __init__(self, num, name, height, age):
+		self.num = num
 		self.name = name
 		self.height = height
 		self.age = age
 
-plant1 = plant("potato", 20, 50)
+plant1 = plant(1, "potato", 20, 50)
+plant2 = plant(2, "tomato", 14, 24)
+plant3 = plant(3, "banana", 420, 140)
 
-print(f"{plant1.name}: {plant1.height}cm, {plant1.age} days old")
+def ft_garden_data():
+	for i in range(i, 4)
+		print(f"{plant1.name}: {plant1.height}cm, {plant1.age} days old")
