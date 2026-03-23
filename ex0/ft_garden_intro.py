103a8c8066491c8c1984f86e28192275bf608ce9
Koodal Kweon <kkweon@f0r4s14.codam.nl>
2026-03-23 17:53:10 +0100

    ft_garden_intro

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,10 @@
+def ft_garden_intro():
+    print("=== Welcome to My Garden ===")
+    print("Plant: Rose")
+    print("height: 25cm")
+    print("Age: 30 days\n")
+    print("=== End of Program ===")
+
+if __name__ == "__main__":
+    ft_garden_intro()
+
