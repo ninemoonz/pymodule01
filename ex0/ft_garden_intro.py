1c6f17e2ce9d277cf63e75c092bad95cfba23637
Koodal Kweon <kkweon@f0r3s14.codam.nl>
2026-03-24 15:46:03 +0100

    fix format to accord with flake8

--- ex0/ft_garden_intro.py
+++ ex0/ft_garden_intro.py
@@ -1,10 +1,10 @@
 def ft_garden_intro():
-	plant = "Rose"
-	height = 25
-	age = 30
-	print("=== Welcome to My Garden ===")
-	print(f"Plant: {plant}")
-	print(f"height: {height} cm")
-	print(f"Age: {age} days")
-	print("\n")
-	print("=== End of Program ===")
+    plant = "Rose"
+    height = 25
+    age = 30
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {plant}")
+    print(f"height: {height} cm")
+    print(f"Age: {age} days")
+    print("\n")
+    print("=== End of Program ===")
--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -1,14 +1,21 @@
 class plant:
-	def __init__(self, num, name, height, age):
-		self.num = num
-		self.name = name
-		self.height = height
-		self.age = age
+    def __init__(self, name, height, age):
+        self.name = name
+        self.height = height
+        self.age = age
 
-plant1 = plant(1, "potato", 20, 50)
-plant2 = plant(2, "tomato", 14, 24)
-plant3 = plant(3, "banana", 420, 140)
+
+plant1 = plant("potato", 20, 50)
+plant2 = plant("tomato", 14, 24)
+plant3 = plant("banana", 420, 140)
+plants = [plant1, plant2, plant3]
+
 
 def ft_garden_data():
-	for i in range(i, 4)
-		print(f"{plant1.name}: {plant1.height}cm, {plant1.age} days old")
+    i = 0
+    for i in range(3):
+        print(
+            f"{plants[i].name}: "
+            f"{plants[i].height}cm, "
+            f"{plants[i].age} days old"
+            )
