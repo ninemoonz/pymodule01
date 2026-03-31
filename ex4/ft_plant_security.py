c74226a8e2828df965fde191034fb096b45855d3
Koodal Kweon <kkweon@f0r5s15.codam.nl>
2026-03-31 16:39:36 +0200

    getter setter confusing

--- ex4/ft_plant_security.py
+++ ex4/ft_plant_security.py
@@ -1,13 +1,21 @@
 class SecurePlant:
-	def __init__(self, name: str, height: int, age: int):
+	def __init__(self, name: str, height: int):
+		self.name = name
+		self._height = height
 
-	def set_height():
-	
-	def set_age():
+	def get_height(self):
+		if self._height > 0:
+			return self.height
+		else:
+			print(f"Invalid operation attempted: height {self._height}cm [REJECTED]")
 
-	
-
+	def set_height(self, x):
+		self._height = get_height(x)
 
 def ft_garden_security():
+	plant1 = SecurePlant("tomato", 0)
+	plant1.get_height(22)
+	print(plant1._height)
+	print(plant1.get_height)
 
-	
+ft_garden_security()
