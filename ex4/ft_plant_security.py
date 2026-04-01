2822970b29725113ee3640be24e238cc08e65da2
Koodal Kweon <kkweon@f1r4s7.codam.nl>
2026-04-01 15:19:19 +0200

    variable security getter setter

--- ex4/ft_plant_security.py
+++ ex4/ft_plant_security.py
@@ -1,21 +1,44 @@
 class SecurePlant:
-	def __init__(self, name: str, height: int):
-		self.name = name
-		self._height = height
+    def __init__(self, height: int, age: int):
+        self._height = 0
+        self._age = 0
 
-	def get_height(self):
-		if self._height > 0:
-			return self.height
-		else:
-			print(f"Invalid operation attempted: height {self._height}cm [REJECTED]")
+    def set_height(self, x: int):
+        self._height = x
 
-	def set_height(self, x):
-		self._height = get_height(x)
+    def get_height(self):
+        if self._height > 0:
+            return print(f"Height updated: {self._height}cm [OK]")
+        else:
+            return print(
+                f"Invalid operation attempted: "
+                f"height {self._height}cm [REJECTED]\n"
+                f"Security: Negative height rejected"
+                )
+
+    def set_age(self, y: int):
+        self._age = y
+
+    def get_age(self):
+        if self._age >= 0:
+            return print(f"Age updated: {self._age} days [OK]")
+        else:
+            return print(
+                f"Invalid operation attempted: "
+                f"age {self._age} days [REJECTED]\n"
+                f"Security: Negative age rejected"
+                )
+
+
+plant1 = SecurePlant(0, 0)
+plant1.set_height(12)
+plant1.set_age(10)
+
 
 def ft_garden_security():
-	plant1 = SecurePlant("tomato", 0)
-	plant1.get_height(22)
-	print(plant1._height)
-	print(plant1.get_height)
+    print('=== Garden Security System ===')
+    plant1.get_height()
+    plant1.get_age()
+
 
 ft_garden_security()
