ffad76b730147fc72388c6f68e92bdf98a245c16
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2026-04-02 23:20:29 +0200

    ft_plant_types about inheritance

---
+++ ex5/ft_plant_types.py
@@ -0,0 +1,11 @@
+class Plant:
+	def __init(self, name: str, height: int, age: int)
+		self.name = name
+		self.height = height
+		self.age = age
+
+class Flower(Plant):
+	def __init__(self, color: str)
+		super().__init__(color)
+	
+	def bloom():
