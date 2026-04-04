6cb431b9d60874395a35aedcb509f65e598e16ea
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2026-04-04 14:29:56 +0200

    create ft_plant_security.py file

---
+++ ex6/ft_garden_analytics.py
@@ -0,0 +1,10 @@
+
+if __name__ == "__main__":
+	def ft_garden_analytics():
+		print("=== Garden Management System Demo")
+		print()
+		#Added plants
+		print()
+		#grow plants
+		print()
+		print("=== Alice's Garden Report ===")
