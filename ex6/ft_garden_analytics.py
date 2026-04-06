63d0d47727b942ad81de39667a2e991b8045c400
Koodal Kweon <kkweon@f0r4s11.codam.nl>
2026-04-06 14:45:35 +0200

    create classes

--- ex6/ft_garden_analytics.py
+++ ex6/ft_garden_analytics.py
@@ -1,10 +1,28 @@
+class Plant:
+	def __init__(self, name: str, height: int) -> None:
+		self.name = name
+		self.height = height
+
+
+class FloweringPlant(Plant):
+	def __init__(self, name: str, height: int, color: str):
+		super().__init__(name, height)
+		self.color = color
+
+
+class PrizeFlower(FloweringPlant):
+	def __init__(self, name: str, height: int, color: str, prize: int):
+		super().__init__(name, height, color)
+		self.prize = prize
+
+
+class GardenManager:
+	def __init__(self, manager_name: str):
+		self.manager_name = manager_name
+
 
 if __name__ == "__main__":
 	def ft_garden_analytics():
-		print("=== Garden Management System Demo")
-		print()
-		#Added plants
-		print()
-		#grow plants
-		print()
-		print("=== Alice's Garden Report ===")
+		...
+
+	ft_garden_analytics()
