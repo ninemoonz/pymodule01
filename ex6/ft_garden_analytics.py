58e82578548c6d20ecc159b8b860cb7aec61c1fe
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2026-04-08 05:11:20 +0200

    staticmethod

--- ex6/ft_garden_analytics.py
+++ ex6/ft_garden_analytics.py
@@ -4,12 +4,12 @@ class Plant:
 		self.height = height
 		self.initial_height = height
 		
-	def grow(self):
-		self.height += 1
-		print(f"{self.name} grew 1cm")
+	def grow(self, grow: int) -> None:
+		self.height += grow
+		print(f"{self.name} grew {grow}cm")
 
-	def get_info(self):
-		return f"{self.name}: {self.height}"
+	def get_info(self) -> str:
+		return f"{self.name}: {self.height}cm"
 
 
 class FloweringPlant(Plant):
@@ -21,7 +21,7 @@ class FloweringPlant(Plant):
 	def bloom(self) -> None:
 		self.is_blooming = True
 
-	def get_info(self) -> None:
+	def get_info(self) -> str:
 		if self.is_blooming:
 			status = "blooming"
 		else:
@@ -32,7 +32,7 @@ class FloweringPlant(Plant):
 class PrizeFlower(FloweringPlant):
 	def __init__(self, name: str, height: int, color: str, prize: int) -> None:
 		super().__init__(name, height, color)
-		self.prize = height / 2
+		self.prize = prize
 
 class Garden:
 	def __init__(self, owner: str) -> None:
@@ -43,13 +43,17 @@ class Garden:
 		self.plants.append(plant)
 		print(f"Added {plant.name} to {self.owner}'s garden")
 	
-	def grow_all(self) -> None:
+	def grow_all(self, grow: int) -> None:
 		print(f"{self.owner} is helping all plants grow...")
 		for plant in self.plants:
-			plant.grow()
+			plant.grow(grow)
+
 
 	def get_report(self) -> None:
-		...
+		print(f"=== {self.owner}'s Garden Report ===")
+		print("Plants in garden:")
+		for plant in self.plants:
+			print(f"- {plant.get_info()}")
 
 	
 class GardenManager:
@@ -60,7 +64,8 @@ class GardenManager:
 		self.gardens.append(garden)
 
 	class GardenStats:
-		...
+		def __init__(self) -> None:
+			...
 
 	@classmethod
 	def create_garden_network(cls, name: list) -> list:
@@ -68,13 +73,24 @@ class GardenManager:
 	
 	@staticmethod
 	def is_valid_height(height: int) -> bool:
-		...
+		return height >= 0
 
 
 if __name__ == "__main__":
 	def ft_garden_analytics():
 		print("=== Garden Management System Demo")
 		print()
-		
+		tyler = Garden("Tyler")
+		tree = Plant("Oak tree", 340)
+		rose = FloweringPlant("Rose", 20, "red")
+		sun = PrizeFlower("Tulip", 30, "blue", 40)
+		tyler.add_plant(tree)
+		tyler.add_plant(rose)
+		tyler.add_plant(sun)
+		print()
+		tyler.grow_all(3)
+		print()
+		tyler.get_report()
+
 
 	ft_garden_analytics()
