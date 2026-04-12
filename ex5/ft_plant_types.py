ee9c6754bdc5d8ac879572fad66767020e3c89b7
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2026-04-12 16:29:10 +0200

    float values

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -5,7 +5,8 @@ class Plant:
         self.age = age
 
     def show(self) -> None:
-        print(f"{self.name}: {self.height}cm, {self.age} days old")
+        print(f"{self.name.capitalize()}: {self.height:.1f}cm, "
+              f"{self.age} days old")
 
 
 class Flower(Plant):
@@ -13,10 +14,6 @@ class Flower(Plant):
         super().__init__(name, height, age)
         self.color = color
         self.bloom_state = False
-
-    def flower_info(self) -> None:
-        print(f"{self.name} (Flower): {self.height}cm, "
-              f"{self.age} days, {self.color} color")
 
     def bloom(self) -> None:
         self.bloom_state = True
@@ -26,9 +23,9 @@ class Flower(Plant):
         super().show()
         print(f"Color: {self.color}")
         if self.bloom_state:
-            print(f"{self.name} is blooming beautifully!")
+            print(f"{self.name.capitalize()} is blooming beautifully!")
         else:
-            print(f"{self.name} has not bloomed yet")
+            print(f"{self.name.capitalize()} has not bloomed yet")
 
 
 class Tree(Plant):
@@ -36,21 +33,17 @@ class Tree(Plant):
         super().__init__(name, height, age)
         self.trunk_diameter = trunk_diameter
 
-    def tree_info(self) -> None:
-        print(f"{self.name} (Tree): {self.height}cm, "
-              f"{self.age} days, {self.trunk_diameter}cm diameter")
-
     def produce_shade(self) -> None:
         print(f"[asking the {self.name} to produce shade]")
-        shade = (self.trunk_diameter / 100) * (self.height / 100)
         print(
-                f"{self.name} now produces a shade of {shade:.0f}cm long and "
-                f"{self.trunk_diameter}cm wide"
+                f"Tree {self.name.capitalize()} now produces a shade of "
+                f"{self.height:.1f}cm long and "
+                f"{self.trunk_diameter:.1f}cm wide."
             )
 
     def show(self):
         super().show()
-        print(f"Trunk diameter: {self.trunk_diameter} cm")
+        print(f"Trunk diameter: {self.trunk_diameter:.1f} cm")
 
 
 class Vegetable(Plant):
@@ -64,10 +57,6 @@ class Vegetable(Plant):
         self.harvest_season = harvest_season
         self.nutritional_value = 0
 
-    def vegetable_info(self) -> None:
-        print(f"{self.name} (Vegetable): {self.height}cm, "
-              f"{self.age} days, {self.harvest_season} harvest")
-
     def vegetable_nut(self, grow_days: int) -> None:
         print(f"[make {self.name} grow and age for {grow_days} days]")
         self.nutritional_value += grow_days
@@ -79,26 +68,29 @@ class Vegetable(Plant):
 
 
 if __name__ == "__main__":
-    rose = Flower("Rose", 20, 14, "red")
-    tulip = Flower("Tulip", 24, 30, "pink")
+    rose = Flower("rose", 20, 14, "red")
+    tulip = Flower("tulip", 24, 30, "pink")
 
-    oak = Tree("Oak", 400, 240, 30)
-    ginkgo = Tree("Ginkgo", 320, 120, 19)
+    oak = Tree("oak", 400, 240, 30)
+    ginkgo = Tree("ginkgo", 320, 120, 19)
 
-    carrot = Vegetable("Carrot", 14, 45, "November")
-    broccoli = Vegetable("Broccoli", 10, 20, "May")
+    carrot = Vegetable("carrot", 14, 45, "November")
+    broccoli = Vegetable("broccoli", 10, 20, "May")
 
-    print("=== Garden Plant Types ===")
-    print("=== Flower")
-    rose.show()
-    rose.bloom()
-    rose.show()
-    print()
-    print("=== Tree")
-    oak.show()
-    oak.produce_shade()
-    print()
-    print("=== Vegetable")
-    carrot.show()
-    carrot.vegetable_nut(20)
-    carrot.show()
+    def garden_plant_types(flower: Flower, tree: Tree, vegetable: Vegetable):
+        print("=== Garden Plant Types ===")
+        print("=== Flower")
+        flower.show()
+        flower.bloom()
+        flower.show()
+        print()
+        print("=== Tree")
+        tree.show()
+        tree.produce_shade()
+        print()
+        print("=== Vegetable")
+        vegetable.show()
+        vegetable.vegetable_nut(20)
+        vegetable.show()
+
+    garden_plant_types(rose, ginkgo, broccoli)
